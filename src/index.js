62dda03eef48c12070ffe8be421e4fb0b8b55ffb
mukesh800 <19570733-mukesh800@users.noreply.replit.com>
2024-07-12 11:32:39 +0000

    Describe your changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,8 +12,6 @@ root.render(
   </React.StrictMode>
 );
 
-// Change this line to register the service worker
-serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
